2284dbba02ec866a7d1dc5343e3f28ae362e8355
Mohammed Azarin vp <azarcoder@gmail.com>
2026-01-18 13:21:09 +0530

    dsaa

--- LINKED-LIST/merge-sorted.js
+++ LINKED-LIST/merge-sorted.js
@@ -1,4 +1,4 @@
-import { printingNode, toLinkedList } from "./utils/utils.js";
+import { Node, printingNode, toLinkedList } from "./utils/utils.js";
 
 let arr1 = [1, 2, 4];
 let arr2 = [1, 3, 4];
@@ -6,12 +6,25 @@ let arr2 = [1, 3, 4];
 let listNode1 = toLinkedList(arr1);
 let listNode2 = toLinkedList(arr2);
 
-let curr = listNode1;
+let dummy = new Node(-1);
+let curr = dummy;
+let link1 = listNode1;
+let link2 = listNode2;
 
-while (curr.next !== null) {
-  curr = curr.next;
+while (link1 !== null && link2 !== null) {
+  if (link1.data <= link2.data) {
+    curr.next = link1;
+    link1 = link1.next;
+  } else {
+    curr.next = link2;
+    link2 = link2.next;
+  }
+  curr=curr.next
 }
-curr.next = listNode2;
+if(link1!==null) curr.next=link1
 
-let res=printingNode(listNode1)
-console.log(res)
+if(link2!==null) curr.next=link2
+
+//printing list
+let res = printingNode(dummy.next);
+console.log(res);
